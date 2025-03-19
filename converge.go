7232d0873ed7a87e41c0dee5708ce1301dcf65a8
Thalys Farias da Silva <65621610+ThalysSilva@users.noreply.github.com>
2025-03-18 22:05:46 -0300

    Create converge.go

---
+++ converge.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	canal := converge(trabalho("maçã"), trabalho("pêra"))
+	for x := 0; x < 16; x++ {
+		fmt.Println(<-canal)
+	}
+
+}
+
+func trabalho(nome string) chan string {
+	canal := make(chan string)
+	go func(nome string, c chan string) {
+		for i := 1; ; i++ {
+			c <- fmt.Sprintf("Função %v diz: %v", nome, i)
+			time.Sleep(time.Millisecond * time.Duration(rand.Intn(1e3)))
+		}
+	}(nome, canal)
+	return canal
+}
+
+func converge(canal1, canal2 chan string) chan string {
+	novoCanal := make(chan string)
+	go func() {
+		for {
+			novoCanal <- <-canal1
+		}
+	}()
+	go func() {
+		for {
+			novoCanal <- <-canal2
+		}
+	}()
+	return novoCanal
+}
